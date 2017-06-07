3f0b06821343c0731b28b22ce4629128003fc82e
Refan <a65091230@hotmail.com>
2017-06-07 18:42:09 +0800

    login page edit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -11,12 +11,13 @@
 <body>
   <div class="wrapper">
 	<div class="container">
+		<div class="logo_img"><img src="/images/logo.png" width="300"></div><!--300*100-->
 		<h1>Welcome</h1>
-		
-		<form class="form">
+		<form id="form" class="form" method="POST" action="">
 			<input type="text" placeholder="Username">
 			<input type="password" placeholder="Password">
-			<button type="submit" id="login-button">Login</button>
+			<button class="g-recaptcha" data-sitekey="6Le0eCQUAAAAAHyEKFXoYyBl8JKolgt5iFWtfRVe" data-badge="inline" data-callback="onSubmit">Login</button>
+			<!--<button type="submit" id="login-button">Login</button>-->
 		</form>
 	</div>
 	
@@ -35,13 +36,16 @@
 </div>
     {!!Html::script('/assets/js/jquery-1.10.2.js')!!}
 
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+
     <script>
-    $("#login-button").click(function(event){
-        event.preventDefault();
-        
-        $('form').fadeOut(500);
+	function onSubmit(token) {
+		$('form').fadeOut(500);
         $('.wrapper').addClass('form-success');
-    });
+		setTimeout(function() {
+			document.getElementById("form").submit();
+		}, 1500);
+	}
     </script>
 
 </body>
